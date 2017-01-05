4c89708b5a1c64046185febde72e9ecde144303f
zsy <zhengshengyang@njrobot.com>
2017-01-05 16:19:51 +0800

    change example demo name

--- tools/build_without_lib_update.sh
+++ tools/build_without_lib_update.sh
@@ -1,1 +1,16 @@
 #!/usr/bin/env bash
+
+set -e
+script=$(readlink -f "$0")
+route=$(dirname "$script")
+
+which cmake >/dev/null 2>&1 || { echo "build requires cmake but nothing is found"; exit 1; }
+
+if [ -d "$route"/../build ]; then
+    rm -rf "$route"/../build
+fi
+
+mkdir -p "$route"/../build || exit 2
+cd "$route"/../build
+cmake .. -G "Unix Makefiles"
+make -j1
--- tools/lib_update.sh
+++ tools/lib_update.sh
@@ -1,1 +1,16 @@
 #!/usr/bin/env bash
+
+#-------------------------------------
+# Filename: lib_update.sh
+# Revision: 1.0
+# Data: 2017/01/05
+# Des: build Dependent libraries
+# Env: Ubuntu 16.04.1
+#-------------------------------------
+
+set -e
+script=$(readlink -f "$0")
+route=$(dirname "$script")
+
+## init zeromq
+## TODO
