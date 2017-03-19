93c211a4e328e890dfe01613794c04a5c203d3f3
echopairs <736418319@qq.com>
2017-03-19 08:59:25 -0700

    Update lib_update.sh to init zeromq

--- tools/lib_update.sh
+++ tools/lib_update.sh
@@ -12,5 +12,31 @@ set -e
 script=$(readlink -f "$0")
 route=$(dirname "$script")
 
-## init zeromq
-## TODO
+
+if [ -d "$route/install_zmq" ]; then
+   sudo  rm -rf $route/install_zmq
+fi
+mkdir $route/install_zmq
+cd $route/install_zmq
+# build zmp
+git clone https://github.com/zeromq/libzmq
+cd libzmq && mkdir build
+cd build
+cmake ../ || exit 1
+make -j 4 || exit 2
+make test || exit 3
+
+sudo make install
+#sudo checkinstall -D --install=yes --fstrans=no --default --pkgname=zmq --pkgversion=4.2.2 --pakdir=$route || exit 4
+# build zmqc++
+cd $route/install_zmq
+git clone https://github.com/zeromq/cppzmq.git
+cd cppzmq && mkdir build
+cd build
+cmake ../ || exit 5
+make -j 4 || exit 6
+#sudo checkinstall -D --install=yes --fstrans=no --default --pkgname=zmq++ --pkgversion=4.2.2 --pakdir=$route
+
+sudo make install
+
+rm -rf install_zmq
